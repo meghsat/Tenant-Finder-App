389bb0aede197d662c31465c9180777d924cc1d9
Megha Sri Satya Sai Devineni <meghasri.devineni@gmail.com>
2022-04-12 11:30:47 +0530

    design changes and owner side user profile update done

--- app/src/main/java/com/example/tenantfinder_new/OwnerActivity.java
+++ app/src/main/java/com/example/tenantfinder_new/OwnerActivity.java
@@ -3,6 +3,8 @@ package com.example.tenantfinder_new;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +16,8 @@ public class OwnerActivity extends AppCompatActivity {
 Button addnewproperty;
 List<List<String>> propertiesdata;
     RecyclerView rvContacts;
+    ImageView userprofile;
+    TextView welcomeuser;
 void method2(List<List<String>> lists)
 {
     TenantpropertieshomeActivity adapterobj=new TenantpropertieshomeActivity(lists,"owner");
@@ -27,7 +31,16 @@ void method2(List<List<String>> lists)
         addnewproperty=(Button) findViewById(R.id.addnewproperty);
         Intent homeintent = getIntent();
         String usernamee = homeintent.getStringExtra("username");
+        welcomeuser=(TextView) findViewById(R.id.welcomeuser);
+        userprofile=(ImageView) findViewById(R.id.userprofile);
 
+        userprofile.setOnClickListener((view2)->{
+            System.out.println("boo");
+            Intent userprofileintent=new Intent(OwnerActivity.this,UserprofileActivity.class);
+            userprofileintent.putExtra("username",usernamee);
+            startActivity(userprofileintent);
+        });
+        welcomeuser.setText("Welcome "+usernamee);
         addnewproperty.setOnClickListener((view)->
         {
 
@@ -37,7 +50,6 @@ void method2(List<List<String>> lists)
             startActivity(addproperty);
         });
          rvContacts = (RecyclerView) findViewById(R.id.rvownerproperties);
-        System.out.println("---------");
 
         PropertiesDataActivity.ownerside(usernamee, new Returnpropertiesdata() {
             @Override
