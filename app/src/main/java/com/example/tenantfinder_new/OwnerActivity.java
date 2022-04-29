1b66e7a2ef1ad30ba09aea74781f171025e49431
Megha Sri Satya Sai Devineni <meghasri.devineni@gmail.com>
2022-04-29 10:53:42 +0530

    made owner page dynamic
    Added city and locality spinners to make the owner page dynamic, now properties of a user gets displayed based on city and locality

--- app/src/main/java/com/example/tenantfinder_new/OwnerActivity.java
+++ app/src/main/java/com/example/tenantfinder_new/OwnerActivity.java
@@ -1,9 +1,14 @@
 package com.example.tenantfinder_new;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +23,9 @@ List<List<String>> propertiesdata;
     RecyclerView rvContacts;
     ImageView userprofile;
     TextView welcomeuser;
+    Spinner city,locality;
+
+
 void method2(List<List<String>> lists)
 {
     TenantpropertieshomeActivity adapterobj=new TenantpropertieshomeActivity(lists,"owner");
@@ -33,7 +41,12 @@ void method2(List<List<String>> lists)
         String usernamee = homeintent.getStringExtra("username");
         welcomeuser=(TextView) findViewById(R.id.welcomeuser);
         userprofile=(ImageView) findViewById(R.id.userprofile);
-
+        city = (Spinner) findViewById(R.id.city);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        city.setAdapter(adapter2);
+        locality = (Spinner) findViewById(R.id.locality);
         userprofile.setOnClickListener((view2)->{
             System.out.println("boo");
             Intent userprofileintent=new Intent(OwnerActivity.this,UserprofileActivity.class);
@@ -50,16 +63,70 @@ void method2(List<List<String>> lists)
             startActivity(addproperty);
         });
          rvContacts = (RecyclerView) findViewById(R.id.rvownerproperties);
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = adapterView.getContext();
+                if((city.getSelectedItem().toString()).equals("Hyderabad"))
+                {
+                    ArrayAdapter<CharSequence>   adapter3 = ArrayAdapter.createFromResource(context,
+                            R.array.Hyderabad_array, android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    locality.setAdapter(adapter3);
+                }
+                else if((city.getSelectedItem().toString()).equals("Vijayawada"))
+                {
+                    ArrayAdapter<CharSequence>  adapter3 = ArrayAdapter.createFromResource(context,
+                            R.array.vijayawada_array, android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    locality.setAdapter(adapter3);
+                }
+                else if((city.getSelectedItem().toString()).equals("Bangalore"))
+                {
+                    ArrayAdapter<CharSequence>  adapter3 = ArrayAdapter.createFromResource(context,
+                            R.array.Bangalore_array, android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    locality.setAdapter(adapter3);
+                }
+                else if((city.getSelectedItem().toString()).equals("Guntur"))
+                {
+                    ArrayAdapter<CharSequence>  adapter3 = ArrayAdapter.createFromResource(context,
+                            R.array.Guntur_array, android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    locality.setAdapter(adapter3);
+                }
+                else
+                {
+                    ArrayAdapter<CharSequence>  adapter3 = ArrayAdapter.createFromResource(context,
+                            R.array.Guntur_array, android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    locality.setAdapter(adapter3);
+                }
+                locality.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+                {
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        Context context = adapterView.getContext();
+                        PropertiesDataActivity.ownerside(usernamee,(city.getSelectedItem().toString()), (locality.getSelectedItem().toString()), new Returnpropertiesdata() {
+                            @Override
+                            public void onSuccess(List<List<String>> lists2) {
+                                propertiesdata = lists2;
+                                System.out.println(propertiesdata);
+                                method2(propertiesdata);
+                            }
 
-        PropertiesDataActivity.ownerside(usernamee, new Returnpropertiesdata() {
-            @Override
-            public  void onSuccess(List<List<String>> lists2){
-                propertiesdata=lists2;
-                System.out.println(propertiesdata);
-       method2(propertiesdata);
+                        });
+                    }
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                        return;
+                    }});
             }
 
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
         });
+
+
 
 //        TenantpropertieshomeActivity adapterobj=new TenantpropertieshomeActivity(propertiesdata,"owner");
 //        rvContacts.setAdapter(adapterobj);
--- app/src/main/java/com/example/tenantfinder_new/PropertiesDataActivity.java
+++ app/src/main/java/com/example/tenantfinder_new/PropertiesDataActivity.java
@@ -56,73 +56,14 @@ static FirebaseStorage storage;
             }
 
         });
-//    List<List<String>> lists = new ArrayList<>();
-//    ArrayList<String> list=new ArrayList<String>();
-//        list.add("Rs 1,00,000 / Month");
-//        list.add("Semi Furnished, 1BHK Flat, 1200 sqft.");
-//        list.add("Flat no:402, 4th Floor, 5th X roads, infront of Forum Mall, HSR Layout, Bengaluru.");
-//        list.add("Owner: Mr. Venkatraya");
-//
-//        lists.add(list);
-//    ArrayList<String> list2=new ArrayList<String>();
-//        list2.add("Rs 15,000 / Month");
-//        list2.add("Fully Furnished, 3BHK Flat, 2500 sqft.");
-//        list2.add("Flat no:500, 5th Floor, RTX cross roads, infront of My friend house, Madhapur, Hyderabad.");
-//        list2.add("Owner: Mr. Satya Sai");
-//
-//        lists.add(list2);
-//    ArrayList<String> list3=new ArrayList<String>();
-//        list3.add("Rs 1,20,000 / Month");
-//        list3.add("Fully Furnished, 6BHK Duplex, 3000 sqft.");
-//        list3.add("Door no:76/15/34, Main Road currency naga, infront of lalita jewllers, Nallamalla forest, AP.");
-//        list3.add("Owner: Mr. happy");
-//
-//        lists.add(list3);
-//
-//        ArrayList<String> list4=new ArrayList<String>();
-//        list4.add("Rs 20,000 / Month");
-//        list4.add("Not Furnished, 2 BHK Flat, 900 sqft.");
-//        list4.add("Flat no:1, First Floor, Mussoorie, infront of starbucks dosas, forest road, Mussoorie.");
-//        list4.add("Owner: Mr. Tony Stark");
-//
-//        lists.add(list4);
-//        ArrayList<String> list5=new ArrayList<String>();
-//        list5.add("Rs 98,876 / Month");
-//        list5.add("Fully Furnished, 5BHK Duplex, 2000 sqft.");
-//        list5.add("Door no:1/2/3, FDS nagar, Beside Trendfet Mall, Top of Hill, Delhi.");
-//        list5.add("Owner: Mr. Ned Stark");
-//
-//        lists.add(list5);
-//        ArrayList<String> list6=new ArrayList<String>();
-//        list6.add("Rs 50,900 / Month");
-//        list6.add("Semi Furnished, 3BHK Appartment, 2800 sqft.");
-//        list6.add("Door:56-56-78, Beverly Road, Beside tony stark house, jubliee hills, Hyderabad.");
-//        list6.add("Owner: Mr. Chris Evans");
-//
-//        lists.add(list6);
-//        ArrayList<String> list7=new ArrayList<String>();
-//        list7.add("Rs 10,000 / Week");
-//        list7.add("Not Furnished, 1BHK Appartment, 1800 sqft.");
-//        list7.add("Flat No:1/3, 4th cross road, Electricity colony, Mega town ship road, inside lake, kerala.");
-//        list7.add("Owner: Mr. Ken kaneki");
-//
-//        lists.add(list7);
-//
-//        ArrayList<String> list8=new ArrayList<String>();
-//        list8.add("Rs 16,000 / Week");
-//        list8.add("Semi Furnished, 3BHK Appartment, 700 sqft.");
-//        list8.add("Flat No:5/67/89, Waterfall street, Ayyappa nagar town ship, kondareddy burj centre, Chittoor.");
-//        list8.add("Owner: Mr. Shoto Todoroki");
-//
-//        lists.add(list8);
         return lists;
 }
-public static void ownerside(String owner,final Returnpropertiesdata listener){
+public static void ownerside(String owner,String City, String locality,final Returnpropertiesdata listener){
 
     rootNode=FirebaseDatabase.getInstance();
     reference=rootNode.getReference().child("properties");
     rootNode=FirebaseDatabase.getInstance();
-    reference=rootNode.getReference().child("properties").child("Bangalore").child("HSRLayout").child(owner);
+    reference=rootNode.getReference().child("properties").child(City).child(locality).child(owner);
     System.out.println("--------------------------------");
     List<List<String>> lists = new ArrayList<>();
     reference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -134,29 +75,8 @@ public static void ownerside(String owner,final Returnpropertiesdata listener){
                 list.add(snapshot.child("furnished").getValue(String.class)+", "+(snapshot.child("bhk").getValue(String.class)).toString()+" BHK "+snapshot.child("type").getValue(String.class)+", "+(snapshot.child("sqft").getValue(String.class)).toString()+" sqft.");
                 list.add(snapshot.child("address").getValue(String.class));
                 list.add("Property Name: "+snapshot.child("propertyname").getValue(String.class));
-                list.add("images/Bangalore/HSRLayout/"+snapshot.child("propertyname").getValue(String.class));
+                list.add("images/"+City+"/"+locality+"/"+snapshot.child("propertyname").getValue(String.class));
                 list.add(owner);
-//                list.add((snapshot.child("rent").getValue(Long.class)).toString());
-//                list.add((snapshot.child("sqft").getValue(Long.class)).toString());
-//                list.add((snapshot.child("type").getValue(String.class)).toString());
-//                list.add(snapshot.child("propertyname").getValue(String.class));
-           // StorageReference ref= storage.getInstance().getReference().child("images/Bangalore/HSRLayout/"+snapshot.child("propertyname").getValue(String.class));
-//            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//                @Override
-//                public void onSuccess(Uri uri) {
-//                    System.out.println(uri);
-//                    list.add(String.valueOf(uri));
-//                }
-//            }).addOnFailureListener(new OnFailureListener() {
-//                @Override
-//                public void onFailure(@NonNull Exception e) {
-//                    list.add(String.valueOf(e));
-//
-//                }
-//            }) ;
-            //   list.add(String.valueOf(storage.getInstance().getReference("images/"+"Bangalore"+"/"+"HSRLayout"+"/"+snapshot.child("propertyname").getValue(String.class))));
-
-//list.add("boo");
                 lists.add(list);
                // System.out.println(lists);
 
